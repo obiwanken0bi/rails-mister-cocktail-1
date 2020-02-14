d981078e73f23e32a8c32b0555a0e280c68bdab8
Alexandre FRASCHINI <alexandre.fraschini@gmail.com>
2020-02-14 17:02:33 +0100

    change asset to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+ # config.assets.compile = false
+  config.assets.compile = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -40,6 +40,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+
+
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
